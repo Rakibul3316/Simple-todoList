b5cd3065819f394cfabdc0f86588d2e910c09fa7
Rakibul Islam <mathrakib3316@gmail.com>
2020-12-15 21:59:16 +0600

    Finished

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-center text-capitalize">todo list</h3>
+            <h3 className="text-center text-capitalize">todo input</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
