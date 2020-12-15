dfd199a7aaed6b406ac8299c9a317c20aa5836fb
Rakibul Islam <mathrakib3316@gmail.com>
2020-12-15 12:23:14 +0600

    some changed on handleSubmit

--- src/App.js
+++ src/App.js
@@ -22,12 +22,16 @@ class App extends Component {
       title: this.state.item,
     };
     const updatedItems = [...this.state.items, newItem];
-    this.setState({
-      items: updatedItems,
-      item: "",
-      id: uuidv4(),
-      editItem: false,
-    });
+
+    this.setState(
+      {
+        items: updatedItems,
+        item: "",
+        id: uuidv4(),
+        editItem: false,
+      },
+      console.log(this.state)
+    );
   };
   clearList = () => {
     this.setState({
